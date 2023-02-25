49c1b661c4ead99640d8cc833641b88bea230220
CJ Amanambu <oamanambu@yahoo.com>
2023-02-25 19:18:47 +0100

    updated loan routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -89,10 +89,10 @@ $routes->post('api/auth/login', 'API\Auth::post_login');
 
 $routes->get('api/accounts', 'API\Account::get_all_account_details', ['filter' => 'authFilter']);
 
-$routes->get('api/loan/(:any)', 'API\Loan::get_loan/$1', ['filter' => 'authFilter']);
+$routes->get('api/loan/(:num)', 'API\Loan::get_loan/$1', ['filter' => 'authFilter']);
 $routes->post('api/loan', 'API\Loan::post_new_loan_application', ['filter' => 'authFilter']);
-$routes->get('api/loans/setups', 'API\Loan::get_loan_setups', ['filter' => 'authFilter']);
-$routes->get('api/loans/outstanding', 'API\Loan::get_outstanding_loans', ['filter' => 'authFilter']);
+$routes->get('api/loan/setups', 'API\Loan::get_loan_setups', ['filter' => 'authFilter']);
+$routes->get('api/loan/outstanding', 'API\Loan::get_outstanding_loans', ['filter' => 'authFilter']);
 
 
 /*
